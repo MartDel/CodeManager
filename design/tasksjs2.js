d15bfd890372eea298daa7663af1da655ddfe528
MartDel <martin-delebecque@outlook.fr>
2020-08-15 01:34:34 +0200

    GG pour le nouveau design frero

--- design/tasksjs2.js
+++ design/tasksjs2.js
@@ -3,19 +3,33 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 var checkbox = document.getElementsByClassName("input_img");
 
+/*
 
+Si tu veux écrire tes fonction avec des technologies plus récentes tu peux les noter :
 
+var nomDeMaFonction = (arg1, arg2) => {
+    mon code...
+}
 
-btn.onclick = function() {
+Au lieu de :
+
+function nomDeMaFonction(arg1, arg2){
+    mon code...
+}
+
+Ca s'appelle ECMAScript et c'est plus conseillé si tu veux être "à la page" :)
+
+ */
+
+btn.onclick = () => {
   modal.style.display = "block";
 }
 
-
-span.onclick = function() {
+span.onclick = () => {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
