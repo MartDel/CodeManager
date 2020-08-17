242d5a9af604e0b9315c75bf9923a1e6cb7adc58
TomMullier <tom.mullier@outlook.fr>
2020-08-17 16:27:44 +0200

    menu finished animation + resol bug width task

--- design/tasksjs2.js
+++ design/tasksjs2.js
@@ -2,7 +2,7 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 var checkbox = document.getElementById("check_js");
-var burger = document.getElementById("burger");
+var burger = document.getElementById("menu_checkbox");
 
 
 btn.onclick = (event) => {
@@ -27,11 +27,13 @@ window.onclick = (event) => {
   }
 };
 
-var menu_opener_count = 1;
+/*var menu_opener_count = 1;*/
+localStorage.setItem('bgcolor', 1);
+/*localStorage.getItem('bgcolor')*/
 
 burger.onclick = () => {
-  if (window["menu_opener_count"] == 1){
-    window["menu_opener_count"]=0;
+  if (localStorage.getItem('bgcolor') == 1){
+    localStorage.setItem('bgcolor', 0);
     /*Suppression des textes*/
     document.getElementById("text_menu_left_1").style.display = "none";
     document.getElementById("text_menu_left_2").style.display = "none";
@@ -56,7 +58,10 @@ burger.onclick = () => {
     document.getElementsByClassName("img_menu_gauche_js")[2].style.marginLeft = "-23px";
     document.getElementsByClassName("img_menu_gauche_js")[3].style.marginLeft = "-23px";
     document.getElementsByClassName("img_menu_gauche_js")[4].style.marginLeft = "-23px";
+    document.getElementsByClassName("ligne_et_taches ")[0].style.marginLeft = "100px";
+    document.getElementsByClassName("ligne_et_taches ")[0].style.width = "calc(100% - 100px)";
     document.getElementById("menu_gauche").style.width="100px";
+    
     document.getElementsByClassName("selectedmenu")[0].style.paddingRight = "0px";
     document.getElementsByClassName("notselectedmenu")[0].style.paddingRight = "0px";
     document.getElementsByClassName("notselectedmenu")[1].style.paddingRight = "0px";
@@ -65,7 +70,7 @@ burger.onclick = () => {
     /*document.getElementById("ligne_haut_tache_id").style.width = "100%";
     document.getElementById("liste_taches").style.width = "1320px";*/
   } else {
-    window["menu_opener_count"]=1;
+    localStorage.setItem('bgcolor', 1);
     /*Ajout des textes*/
     setTimeout(function(){document.getElementById("text_menu_left_1").style.display = "block";},300);
     setTimeout(function(){document.getElementById("text_menu_left_2").style.display = "block";},300);
@@ -90,6 +95,8 @@ burger.onclick = () => {
     document.getElementsByClassName("img_menu_gauche_js")[2].style.marginLeft = "0px";
     document.getElementsByClassName("img_menu_gauche_js")[3].style.marginLeft = "0px";
     document.getElementsByClassName("img_menu_gauche_js")[4].style.marginLeft = "0px";
+    document.getElementsByClassName("ligne_et_taches ")[0].style.marginLeft = "260px";
+    document.getElementsByClassName("ligne_et_taches ")[0].style.width = "calc(100% - 260px)";
     document.getElementById("menu_gauche").style.width="260px";
     /*document.getElementById("myBtn").style.width="calc(100vw - 259px)";*/
     document.getElementsByClassName("selectedmenu")[0].style.paddingRight = "90px";
@@ -100,5 +107,81 @@ burger.onclick = () => {
     document.getElementsByClassName("ligne_haut_tache")[0].style.width="auto";
   }
 }
+
+if (localStorage.getItem('bgcolor') == 1){
+  localStorage.setItem('bgcolor', 0);
+  /*Suppression des textes*/
+  document.getElementById("text_menu_left_1").style.display = "none";
+  document.getElementById("text_menu_left_2").style.display = "none";
+  document.getElementById("text_menu_left_3").style.display = "none";
+  document.getElementById("text_menu_left_4").style.display = "none";
+  document.getElementById("text_menu_left_5").style.display = "none";
+  document.getElementById("text_menu_left_1").style.opacity = "0";
+  document.getElementById("text_menu_left_2").style.opacity = "0";
+  document.getElementById("text_menu_left_3").style.opacity = "0";
+  document.getElementById("text_menu_left_4").style.opacity = "0";
+  document.getElementById("text_menu_left_5").style.opacity = "0";
+  document.getElementById("new_task_text").style.display = "none";
+  document.getElementById("new_task_text").style.opacity = "0";
+  /*document.getElementById("new_task_text").style.visibility = "hidden";*/
+  /*gestion des placements*/
+  document.getElementById("new_task_div").style.paddingBottom = "7px";
+  document.getElementById("new_task_div").style.marginLeft = "23px";
+  document.getElementById("new_task_div").style.width = "50px";
+  document.getElementById("new_task_img").style.marginLeft = "8px";
+  document.getElementsByClassName("img_menu_gauche_js")[0].style.marginLeft = "-23px";
+  document.getElementsByClassName("img_menu_gauche_js")[1].style.marginLeft = "-23px";
+  document.getElementsByClassName("img_menu_gauche_js")[2].style.marginLeft = "-23px";
+  document.getElementsByClassName("img_menu_gauche_js")[3].style.marginLeft = "-23px";
+  document.getElementsByClassName("img_menu_gauche_js")[4].style.marginLeft = "-23px";
+  document.getElementsByClassName("ligne_et_taches ")[0].style.marginLeft = "100px";
+  document.getElementsByClassName("ligne_et_taches ")[0].style.width = "calc(100% - 100px)";
+  document.getElementById("menu_gauche").style.width="100px";
+  
+  document.getElementsByClassName("selectedmenu")[0].style.paddingRight = "0px";
+  document.getElementsByClassName("notselectedmenu")[0].style.paddingRight = "0px";
+  document.getElementsByClassName("notselectedmenu")[1].style.paddingRight = "0px";
+  document.getElementsByClassName("notselectedmenu")[2].style.paddingRight = "0px";
+  document.getElementsByClassName("notselectedmenu")[3].style.paddingRight = "0px";
+} else {
+  localStorage.setItem('bgcolor', 1);
+  /*Ajout des textes*/
+  setTimeout(function(){document.getElementById("text_menu_left_1").style.display = "block";},300);
+  setTimeout(function(){document.getElementById("text_menu_left_2").style.display = "block";},300);
+  setTimeout(function(){document.getElementById("text_menu_left_3").style.display = "block";},300);
+  setTimeout(function(){document.getElementById("text_menu_left_4").style.display = "block";},300);
+  setTimeout(function(){document.getElementById("text_menu_left_5").style.display = "block";},300);
+  setTimeout(function(){document.getElementById("text_menu_left_1").style.opacity = "1";},310);
+  setTimeout(function(){document.getElementById("text_menu_left_2").style.opacity = "1";},310);
+  setTimeout(function(){document.getElementById("text_menu_left_3").style.opacity = "1";},310);
+  setTimeout(function(){document.getElementById("text_menu_left_4").style.opacity = "1";},310);
+  setTimeout(function(){document.getElementById("text_menu_left_5").style.opacity = "1";},310);
+  setTimeout(function(){document.getElementById("new_task_text").style.display = "block";},300);
+  setTimeout(function(){document.getElementById("new_task_text").style.opacity = "1";},310);
+  /*setTimeout(function(){document.getElementById("new_task_text").style.visibility = "visible";},500);*/
+  /*gestion des placements*/
+  setTimeout(function(){document.getElementById("new_task_div").style.paddingBottom = "2px";},300);
+  document.getElementById("new_task_div").style.marginLeft = "23px";
+  document.getElementById("new_task_div").style.width = "210px";
+  document.getElementById("new_task_img").style.marginLeft = "8px";
+  document.getElementsByClassName("img_menu_gauche_js")[0].style.marginLeft = "0px";
+  document.getElementsByClassName("img_menu_gauche_js")[1].style.marginLeft = "0px";
+  document.getElementsByClassName("img_menu_gauche_js")[2].style.marginLeft = "0px";
+  document.getElementsByClassName("img_menu_gauche_js")[3].style.marginLeft = "0px";
+  document.getElementsByClassName("img_menu_gauche_js")[4].style.marginLeft = "0px";
+  document.getElementsByClassName("ligne_et_taches ")[0].style.marginLeft = "260px";
+  document.getElementsByClassName("ligne_et_taches ")[0].style.width = "calc(100% - 260px)";
+  document.getElementById("menu_gauche").style.width="260px";
+  /*document.getElementById("myBtn").style.width="calc(100vw - 259px)";*/
+  document.getElementsByClassName("selectedmenu")[0].style.paddingRight = "90px";
+  document.getElementsByClassName("notselectedmenu")[0].style.paddingRight = "90px";
+  document.getElementsByClassName("notselectedmenu")[1].style.paddingRight = "90px";
+  document.getElementsByClassName("notselectedmenu")[2].style.paddingRight = "90px";
+  document.getElementsByClassName("notselectedmenu")[3].style.paddingRight = "90px";
+  document.getElementsByClassName("ligne_haut_tache")[0].style.width="auto";
+}
+
+
+
 
 
