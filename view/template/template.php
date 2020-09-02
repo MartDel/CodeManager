475ecbb21efef29e2398bf230c2f82c0c3cc599e
MartDel <martin-delebecque@outlook.fr>
2020-09-02 23:19:45 +0200

    Comment Tawk.to script

--- view/template/template.php
+++ view/template/template.php
@@ -13,7 +13,7 @@
 	    <link rel="shortcut icon" href="public/img/programmer2.png">
 	    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 
-	    <!--Start of Tawk.to Script-->
+	    <!--Start of Tawk.to Script
 	    <script type="text/javascript">
 	        var Tawk_API = Tawk_API || {},
 	            Tawk_LoadStart = new Date();
@@ -27,7 +27,7 @@
 	            s0.parentNode.insertBefore(s1, s0);
 	        })();
 	    </script>
-	    <!--End of Tawk.to Script-->
+	    End of Tawk.to Script-->
 
 	</head>
 
