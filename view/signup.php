b8157512202d01447986b5ab05eddcb4eb8c76b8
TomMullier <tom.mullier@outlook.fr>
2020-10-20 12:27:26 +0200

    design sign up page

--- view/signup.php
+++ view/signup.php
@@ -1,26 +1,33 @@
 <?php
 
 $title = "Inscription";
+$cssfile = "signup";
 ob_start();
 
 ?>
+<div class="title_form_container">
+  <h1 class="form_title">Inscription</h1>
+</div>
+<div class="form_container">
+  <form action="index.php?action=checkSignUp" method="post">
+      <div class="name_container_flex">
+        <input class="input_text_name" type="text" name="firstname" placeholder="Prénom" required />
+        <br>
+        <input class="input_text_name" type="text" name="lastname" placeholder="Nom" required />
+        <br><br>
+      </div>
 
-<form action="index.php?action=checkSignUp" method="post">
-    <input type="text" name="firstname" placeholder="Prénom" required />
-    <br>
-    <input type="text" name="lastname" placeholder="Nom" required />
-    <br>
-    <input type="text" name="pseudo" placeholder="Pseudo" required />
-    <br>
-    <input type="mail" name="mail" placeholder="Addresse mail" required />
-    <br>
-    <input type="password" name="password" placeholder="Mot de passe" required />
-    <br>
-    <input type="password" name="confirm" placeholder="Confirmer mot de passe" required />
-    <br>
-    <input type="submit" value="Envoyer" />
-</form>
-
+      <input class="input_text" type="text" name="pseudo" placeholder="Pseudo" required />
+      <br>
+      <input class="input_text" type="mail" name="mail" placeholder="Addresse mail" required />
+      <br>
+      <input class="input_text" type="password" name="password" placeholder="Mot de passe" required />
+      <br>
+      <input class="input_text" type="password" name="confirm" placeholder="Confirmer mot de passe" required />
+      <br><br><br>
+      <input class="button_submit_signup" type="submit" value="S'inscrire" />
+  </form>
+</div>
 <?php
 
 $content = ob_get_clean();
