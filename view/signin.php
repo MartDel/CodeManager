c5b78a30260263374301c3f0331a6e1ec055ddbf
TomMullier <tom.mullier@outlook.fr>
2020-10-20 12:10:47 +0200

    design signin page

--- view/signin.php
+++ view/signin.php
@@ -1,23 +1,31 @@
 <?php
 
-$title = "Connection";
+$title = "Connexion";
+$cssfile = "signin";
 ob_start();
 
 ?>
+<div class="title_form_container">
+  <h1 class="form_title">Connexion</h1>
+</div>
+<div class="form_container">
+  <form action="index.php?action=checkSignIn" method="post">
+    <input class="input_text" type="text" name="login" placeholder="Pseudo / Addresse mail" />
+    <br>
+    <input class="input_text" type="password" name="password" placeholder="Mot de passe" />
+    <br>
+    <div class="label_container">
+      <label>
+        Connexion automatique
+        <input type="checkbox" name="auto" checked />
+      </label>
+    </div>
+    <br>
+    <br>
+    <input class="button_submit_signin" type="submit" value="Se connecter" />
+  </form>
+</div>
 
-<form action="index.php?action=checkSignIn" method="post">
-  <input type="text" name="login" placeholder="Pseudo / Addresse mail" />
-  <br>
-  <input type="password" name="password" placeholder="Mot de passe" />
-  <br>
-  <label>
-    Connexion automatique
-    <input type="checkbox" name="auto" checked />
-  </label>
-  <br>
-  <br>
-  <input type="submit" value="Envoyer" />
-</form>
 
 <?php
 
