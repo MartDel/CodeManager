c5fe7ab3657538b57814f257421688beaca3eaa4
MartDel <martin-delebecque@outlook.fr>
2020-12-11 17:31:29 +0100

    Fix merge problem

--- view/main.php
+++ view/main.php
@@ -134,11 +134,7 @@ ob_start();
                                 <tr class="table_row_main categories">
                                     <td class="table_col_main category-name">
                                       </br>
-<<<<<<< HEAD
-                                      <div class="">
-=======
                                       <div>
->>>>>>> c0a5550754c039ceafbc6a43b318b1347e53a57e
                                         <input type="checkbox" class="to-check2 category-check" id="category<?= $category_id ?>" />
                                         <p>
                                             <label title="<?= getCategoryNameById($category_id) ? getCategoryNameById($category_id) : 'Divers' ?>" for="category<?= $category_id ?>"><?= getCategoryNameById($category_id) ? getCategoryNameById($category_id) : 'Divers' ?></label>
