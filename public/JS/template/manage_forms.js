aea87b026ff128d3d3af5fb05f0283d1ea640277
Martin Delebecque <martin-delebecque@outlook.fr>
2020-12-15 19:46:05 +0100

    Update manage_forms.js

--- public/JS/template/manage_forms.js
+++ public/JS/template/manage_forms.js
@@ -1,33 +1,9 @@
-// Find all of inputs
-let fields = {
-    password: [],
-    mail: [],
-    submit: null
-}
-const inputs = document.getElementsByTagName('input')
-for (let i = 0; i < inputs.length; i++) {
-    switch (inputs[i].type) {
-        case 'password': fields.password.push(inputs[i]); break;
-        case 'email': fields.mail.push(inputs[i]); break;
-        case 'submit': fields.submit = inputs[i]; break;
+$("form").submit(function (e){
+    const $passwords = $("input[type='password']")
+    e.preventDefault()
+    if($passwords.length === 2 && $passwords[0].value !== $passwords[1].value){
+        const err = new Message('error', 'Oups !', 'Les 2 mots de passe saisis sont différents.')
+        err.dynamic = focusPassword
+        err.show()
     }
-}
-
-fields.submit.onclick = (event) => {
-    if(fields.password.length == 2 && fields.password[0].value !== fields.password[1].value){
-        event.preventDefault()
-        showMessage('error', 'Mots de passe différents', 'Les 2 mots de passe saisis sont différents. Veuillez les resaisir.', focusPassword)
-    }
-    if(fields.mail.length > 0){
-        let wrong_mail = false
-        const regex = /\S+@\S+\.\S+/
-        for (let i = 0; i < fields.mail.length; i++) {
-            if(!regex.test(fields.mail[i].value)) wrong_mail = true
-        }
-        console.log(regex.test(fields.mail[0].value));
-        if(wrong_mail){
-            event.preventDefault()
-            showMessage('error', 'E-mail incorrect', "Le format de l'addrese e-mail n'est pas correct. Veuillez la resaisir.", focusEmail)
-        }
-    }
-}
+})
