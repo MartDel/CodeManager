1090cc415765924f62e8e4dd9d6eb5a7ede70ca8
Martin Delebecque <martin-delebecque@outlook.fr>
2020-12-21 16:36:42 +0100

    Init branch

--- public/JS/template/manage_forms.js
+++
@@ -1,9 +0,0 @@
-$("form").submit(function (e){
-    const $passwords = $("input[type='password']")
-    if($passwords.length === 2 && $passwords[0].value !== $passwords[1].value){
-        e.preventDefault()
-        const err = new Message('error', 'Oups !', 'Les 2 mots de passe saisis sont différents.')
-        err.dynamic = focusPassword
-        err.show()
-    }
-})
