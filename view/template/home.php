9938213c8d5942d54ed1097b6b3004a2e44bdcb5
Tom Mullier <tom.mullier@outlook.fr>
2021-01-21 16:21:31 +0100

    meta desc

--- view/template/home.php
+++ view/template/home.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="CodeManager est un site de gestion de projet informatique en équipe. Gérez ce qu'il vous reste à faire, fixez vous des objectifs et ayez une vue d'ensemble sur votre projet avec les divers fonctionnalités de CodeManager">
 		<!-- CSS -->
 		<link rel="stylesheet" type="text/css" href="public/CSS/template/message.css" />
 		<link rel="stylesheet" type="text/css" href="public/CSS/<?= $cssfile ?>.css" />
